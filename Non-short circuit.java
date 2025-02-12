05df527e583fae85eb17a576d019fcdee6fa0a8a
Maharaja2004 <120156771+Maharaja2004@users.noreply.github.com>
2025-02-12 11:47:51 +0530

    Create Non-short circuit.java

---
+++ Non-short circuit.java
@@ -0,0 +1,17 @@
+public class Main
+{
+	public static void main(String[] args) {
+      int a=20;
+      int b=35;
+      int c=43;
+      if(a>b & a>c){
+          System.out.println("a is greater");
+      }
+      else if(b>c){
+          System.out.println("b is greater");
+      }
+      else{
+          System.out.println("c is greater");
+      }
+	}
+}
