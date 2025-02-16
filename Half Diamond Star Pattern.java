e8cd51421ad856ba2560d25688d2ff5e9a163aa6
Maharaja2004 <120156771+Maharaja2004@users.noreply.github.com>
2025-02-16 13:02:41 +0530

    Create Half Diamond Star Pattern.java

---
+++ Half Diamond Star Pattern.java
@@ -0,0 +1,35 @@
+
+
+public class Main
+{
+	public static void main(String[] args) {
+		int n=4;
+		
+		for(int i=1;i<=n;i++){
+		    for(int space=1;space<=n;space++){
+		    System.out.print(" ");
+		}
+		    for(int j=i;j>=1;j--){
+		        System.out.print("*");
+		    }
+		    System.out.println();
+		}
+		for(int i=n-1;i>=1;i--){
+		    for(int space=1;space<=n;space++){
+		    System.out.print(" ");
+		}
+		    for(int j=1;j<=i;j++){
+		        System.out.print("*");
+		    }
+		    System.out.println();
+		}
+	}
+}
+o/p:
+    *
+    **
+    ***
+    ****
+    ***
+    **
+    *
