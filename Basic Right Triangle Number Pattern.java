5223353b44d5f7fc584c3ebc146049f120b8c83c
Maharaja2004 <120156771+Maharaja2004@users.noreply.github.com>
2025-02-16 15:48:50 +0530

    Create Basic Right Triangle Number Pattern.java

---
+++ Basic Right Triangle Number Pattern.java
@@ -0,0 +1,20 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+		int n=4;
+		int count=0;
+		for(int i=1;i<=n;i++){
+		    for(int j=1;j<=i;j++){
+		        count++;
+		        System.out.print(count);
+		    }
+		    System.out.println();
+		}
+	}
+}
+o/p:
+1
+23
+456
+78910
