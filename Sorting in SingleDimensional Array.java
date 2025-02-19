d22301c4db0b77eca352a4749d9a3584f14ecd48
Maharajasivam M <120156771+Maharaja2004@users.noreply.github.com>
2025-02-19 12:21:00 +0530

    Update Sorting in SingleDimensional Array.java

--- Sorting in SingleDimensional Array.java
+++ Sorting in SingleDimensional Array.java
@@ -1,12 +1,11 @@
-
 public class Main
 {
 	public static void main(String[] args) {
 		int arr[]= {3,4,3,4,2,1,0};
 		int temp=0;
 		for(int i=0; i<arr.length; i++) {
-			for(int j=0; j<arr.length; j++) {
-				if(arr[i]<arr[j]) {
+			for(int j=i+1; j<arr.length; j++) {
+				if(arr[i]>arr[j]) {
 					temp=arr[i];
 					arr[i]=arr[j];
 					arr[j]=temp;
